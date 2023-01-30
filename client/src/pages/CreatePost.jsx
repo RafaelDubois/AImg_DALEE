32dbe7160fcb7263491552985a07a352fb4f06ba
RafaelDubois <rafaeldubois974@gmail.com>
2023-01-30 11:50:29 +0100

    added nodemon and backend route render

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('render url dalle', {
+        const response = await fetch('https://aimg-dalee-clone.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('render url /v1/post', {
+        const response = await fetch('https://aimg-dalee-clone.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
